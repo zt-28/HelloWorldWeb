69814e9f5c90f3293245fc929d6801c82066068d
zt-28 <431804936@qq.com>
2021-10-10 15:56:23 +0800

    HelloWorldWeb

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/index")
+    public String web() {
+        String Result = "helloword Spring Boot！这是一个用Spring Boot开发的网站。";
+        return Result;
+    }
+}
